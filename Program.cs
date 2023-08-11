a1c70b3fad86fe1930b360d826376ff8d2c23344
Tony <tony@fl0.com>
2023-08-11 13:38:34 +1000

    update the port

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://*:{port}/");
 // Add services to the container.
 
 builder.Services.AddControllers();
